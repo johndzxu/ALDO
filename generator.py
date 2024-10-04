6589abdd1b610b7043111869afe2a730cf99c6c5
Strelitzia23 <159641171+Strelitzia23@users.noreply.github.com>
2024-10-04 09:47:32 -0400

    see the resized pic.py

--- generator.py
+++ generator.py
@@ -40,8 +40,9 @@ def resize(img):
     new_width, new_height = calculate_new_dimensions(original_width, original_height)
     resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
     # Save the resized image
-    cv2.imwrite("resized_image.jpg", resized_image)
-    
+    cv2.imwrite("resized_image.jpg", resized_image) ##see the pic
+    st.image(resized_image, caption='Result Image', use_column_width=True)
+
     return resized_image
 
 
