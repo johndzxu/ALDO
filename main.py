15041b5c270099a2d7e32b193074ba7934ff0900
Strelitzia23 <159641171+Strelitzia23@users.noreply.github.com>
2024-10-04 09:30:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def main():
 
     person_image = st.camera_input("Take a picture of the person")
 
+    #save to a file
+    if person_image:
+        with open ('input_pic.jpg', 'wb') as file:
+            file.write(person_image.getbuffer())
+
     if shoes_image and person_image:
         st.image(shoes_image, caption='ALDO Shoes', use_column_width=True)
         st.image(person_image, caption='Captured Person Image', use_column_width=True)
 
-        resized_image = resize(person_image)
-        resized_image.save('resized_image.jpg', format='JPEG')
+        resized_image = resize('input_pic.jpg') ###edition here
+        
+        #resized_image.save('resized_image.jpg', format='JPEG')
 
         generator = Generator()
         description = generator.generate_description(shoes_image)
